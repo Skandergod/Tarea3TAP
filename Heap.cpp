1cb1d64ddd70f4239eba6be665a3ff72e12c787e
skandergod <thedanidacosta@gmail.com>
2024-05-14 17:41:35 -0400

    insert added

--- Heap.cpp
+++ Heap.cpp
@@ -3,20 +3,31 @@
 
 
 template <class Elem>
-class heap: std::vector<Elem>
+class heap:std::vector<Elem>
 {
     public:
+        int n;
         heap();
         heap(const heap<Elem> &h);
         heap(const std::vector<Elem> &a);
         ~heap();
-        void insert(const Elem &e);
+        void insert(const Elem &e)
+        {
+            this->insert(e);
+            flotar(this,n);
+            n++;
+        };
         void remove();
         Elem &head(); // retorna referencia
         Elem head() const; // retorna copia
         static void sort(std::vector< Elem > &A); // ordena A con “<“
         void heapify(std::vector< Elem > &A);
         void sink(std::vector< Elem > &A, int k, int n); // hundir
+        void flotar(std::vector< Elem > &A, int k)
+        {
+
+            
+        };
         bool operator< (const Elem& e) const
         {
             return this->at(0) < e;
