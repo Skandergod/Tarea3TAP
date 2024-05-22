77f249d4de1ce03fed0a2b060f824c1119d9bc93
skandergod <thedanidacosta@gmail.com>
2024-05-21 21:56:42 -0400

    fixed stuff

--- Heap.cpp
+++ Heap.cpp
@@ -14,20 +14,54 @@ class heap:std::vector<Elem>
         void insert(const Elem &e)
         {
             this->insert(e);
-            flotar(this,n);
-            n++;
+            flotar(this.n);
+            this.n++;
         };
-        void remove();
+        void remove()
+        {
+            swap( this[1], this[ this.n ]); this.n--;
+            sink( this, 1, this.n);
+        };
         Elem &head(); // retorna referencia
         Elem head() const; // retorna copia
         static void sort(std::vector< Elem > &A); // ordena A con “<“
-        void heapify(std::vector< Elem > &A);
-        void sink(std::vector< Elem > &A, int k, int n); // hundir
-        void flotar(std::vector< Elem > &A, int k)
+        void heapify()
+        {
+            for (int i = this.n/2; i>0; i--)
+                hundir( this, i, this.n);
+        };
+        void sink(int k)
+        {
+            while (2*k <= this.n) 
+            {
+                int j = 2*k;
+                // bajo por el más grande de los hijos
+                // si es un maxheap
+                if (j < this.n && this[j]<this[j+1]) j++;
+                if (this[k] >= this[j]) break;
+                swap( this[k], this[j]);
+                k = j;
+            }
+        }; // hundir
+        void flotar(int k)
         {
 
-            
+            while (k > 1 && this[k/2] < this[k]) 
+            {
+                swap( this[k/2], this[k] );
+                k = k/2;
+            }
+
         };
+        void heapSort() 
+        {
+            heapify( this, this.n);
+            for(int i = this.n; i > 1; i--) 
+            {
+                swap(this[1], this[i]);
+                hundir(this, 1,--this.n);
+            }
+        }
         bool operator< (const Elem& e) const
         {
             return this->at(0) < e;
@@ -40,5 +74,8 @@ class heap:std::vector<Elem>
 
 int main()
 {
+
+    
+
 
 }
